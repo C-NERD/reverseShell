d4d1107084a561809affa2dade75e75d7d6de663
C-NERD <alayaa694@gmail.com>
2021-04-01 18:05:39 +0100

    added comments to the code

--- src/client.py
+++ src/client.py
@@ -1,9 +1,13 @@
+## Importing our dependencies from the python standard library
 from socket import *
 from pickle import loads, UnpicklingError
 
 def client(ip, port):
 
     try:
+        ## Starts the socket instance and connects to the server using ip address $ip on
+        ## port $port. It ensures the connection is successfull else the program
+        ## is terminated.
         client = socket(AF_INET, SOCK_STREAM)
         client.connect((ip, port))
         print('connected to {} at port {}\n'.format(ip, port))
@@ -13,30 +17,43 @@ def client(ip, port):
         quit(-1)
 
     while True:
-
+        ## The programs main loop, it sends commands to the server while making sure
+        ## that the program does not get terminated after receiving output from the server
         command = input('')
-        client.sendall(command.encode('utf-8'))
-
         if command == 'quit':
+            ## Terminates the program when the given command is 'quit'
             print('disconnecting...')
             quit()
-
-        data = client.recv(1024)
-        try:
-            info = loads(data)
-            print('status  :', info[0])
-            print(info[1])
         
-        except:
-            print('status  : failed')
+        else:
+            ## Sends command to the server and displays output and execution status
+            client.sendall(command.encode('utf-8'))
+            data = client.recv(1024)
+            try:
+                info = loads(data)
+                print('status  :', info[0])
+                print(info[1])
+            
+            except:
+                print('status  : failed')
+                continue
 
 if __name__ == '__main__':
+    ## Execution starts here
     ip = input('ipaddress -> ')
 
     try:
+        ## Makes sure the given port is an integer between 1000 and 50000
         port = int(input('port -> '))
+
+        if port < 1000 or port > 50000:
+            print('Error: the port is meant to be a number from 1000 to 50000')
+            quit(-1)
     except:
-        print('Error: the port is meant to be a number from 1000 to 60000')
+        print('port {} is not an integer'.format(port))
         quit(-1)
 
+    ## Invokes the client function with the parameters ip and port.
+    ## This function is responsible for connecting to the server, sending and
+    ## receiving commands to and from the server.
     client(ip, port)
--- src/server.py
+++ src/server.py
@@ -1,8 +1,12 @@
+## Importing the required dependencies
 from socket import *
 from subprocess import getstatusoutput
 from pickle import dumps
 
 def server(ip : str):
+    ## Starts socket instance and makes the socket reuseable,
+    ## binds the socket to ip address $ip on port 5555 then listens
+    ## and accepts connection from one client
     serve = socket(AF_INET, SOCK_STREAM)
     serve.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serve.bind((ip, 5555))
@@ -10,9 +14,10 @@ def server(ip : str):
     conn, addr = serve.accept()
 
     if conn:
-        #print('connected to address ' + str(addr))
-
+        ## If the connection is successfull start the main loop
         while True:
+            ## The main loop receives commands from the connected client,
+            ## executes the commands and replies with the commands output
             data = conn.recv(1024)
             command = data.decode('utf-8')
             
@@ -25,4 +30,8 @@ def server(ip : str):
                 conn.send(dumps(info))
 
 if __name__ == '__main__':
+    ## Execution begins here.
+    ## The server function is invoked, this function is responsible
+    ## for listening to client connections and running commands from
+    ## the said client.
     server('0.0.0.0')
